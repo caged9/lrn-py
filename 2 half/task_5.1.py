836ab5ef60095120b2baf2cb15b28991eaa1c575
caged9 <topankiv@bk.ru>
2021-04-28 16:36:51 +0300

    initiate

---
+++ 2 half/task_5.1.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+k = int(input('Type quantity of enterprises: '))
+#enterprises = {}
+enterprises = defaultdict()
+
+for i in range(1, k + 1):
+        name = input('Type name of enterprise: ')
+        enterprises[name] = [float(input('Profit 1 quarter: ')),
+                             float(input('Profit 2 quarter: ')),
+                             float(input('Profit 3 quarter: ')),
+                             float(input('Profit 4 quarter: '))]
+        enterprises[name].append(enterprises[name][0]+
+                                enterprises[name][1]+
+                                enterprises[name][2]+
+                                enterprises[name][3])
+
+#print(enterprises)
+
+summ = 0
+
+for key, value in enterprises.items():
+    summ += value[4]
+
+
+medprofit = summ / k
+
+print('Medium profit: ', medprofit)
+
+for key, value in enterprises.items():
+    if medprofit < value[4]:
+        print(f'Enterprise {key} have earnings higher than average')
+    if medprofit > value[4]:
+        print(f'Enterprise {key} have earnings lower than average')
+    
---
+++ 2 half/task_5.2.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+k = int(input('Type quantity of enterprises: '))
+# enterprises = {}
+enterprises = defaultdict()
+profit = {}
+
+
+for i in range(k):
+    name = input('Type name of enterprise: ')
+    enterprises[name] = [float(input('Profit 1 quarter: ')),
+                         float(input('Profit 2 quarter: ')),
+                         float(input('Profit 3 quarter: ')),
+                         float(input('Profit 4 quarter: '))]
+
+    profit[i] = enterprises[name][0] + enterprises[name][1] + enterprises[name][2] +enterprises[name][3]
+
+# print(enterprises)
+
+summ = 0
+
+for i in range(k):
+    summ += profit[i]
+
+medprofit = summ / k
+
+print('Medium profit: ', medprofit)
+
+t = 0
+for key, value in enterprises.items():
+    if medprofit < profit[t]:
+        print(f'Enterprise {key} have earnings higher than average')
+    if medprofit > profit[t]:
+        print(f'Enterprise {key} have earnings lower than average')
+    t += 1
